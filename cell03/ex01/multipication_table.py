ddf7359e37efaed77f60932999e7c0007ef9bdf9
CERAMYHGIN <67080191@kmitl.ac.th>
2026-02-11 18:31:42 +0700

    Add files via upload

---
+++ cell03/ex01/multipication_table.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+Num = int(input("Enter your Number: "))
+i = 0
+while i <= 9:
+    print (Num,"x",i,"=",i * Num)
+    i += 1
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+Words = input("What you gonna say: ")
+while True:
+    if Words == "STOP" :
+        break
+    Words = input("I got that! Anything else?: ")
